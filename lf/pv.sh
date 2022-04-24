489e573f4f3f62e49d2244b409fd2c5352a38615
rekaerst <rekaerst@outlook.com>
2022-04-24 11:55:26 +0800

    lf:view md with glow

--- lf/pv.sh
+++ lf/pv.sh
@@ -6,5 +6,6 @@ case "$1" in
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
     *.pdf) pdftotext "$1" -;;
-    *) highlight -O truecolor --force --config-file=$HOME/.config/lf/highlight.theme "$1";;
+    *.md) glow -s dark "$1";;
+    *) highlight -O truecolor --force --config-file="${XDG_CONFIG_HOME:-$HOME/.config}/lf/highlight.theme" "$1";;
 esac
