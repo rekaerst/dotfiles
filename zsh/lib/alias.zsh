25a9dc85e85422945d724542455e5dfec2112204
rekaerst <rekaerst@outlook.com>
2022-05-03 23:46:44 +0800

    chore: remove unnecessary aliases

--- zsh/lib/alias.zsh
+++ zsh/lib/alias.zsh
@@ -11,6 +11,3 @@ alias lsof='lsof -n'
 alias hs="python -m http.server"
 alias R="R --quiet"
 alias rename=perl-rename
-alias nautilus="GTK_THEME=Materia-dark-compact nautilus"
-alias eog="GTK_THEME=Materia-dark-compact eog"
-alias totem="GTK_THEME=Materia-dark-compact totem"
