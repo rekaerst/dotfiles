e08dac13e999e0f61aed055ab3629aad69a498f5
rekaerst <rekaerst@outlook.com>
2022-05-06 08:45:55 +0800

    feat: add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
-cd "${XDG_CONFIG_HOME:-$HOME/.config}" || { 
-	echo "failed to change the working directory"
-	exit 255;
-}
+DOTFILES="${XDG_CONFIG_HOME:-$HOME/.config}/dotfiles"
 
-if [[ ! -d "${XDG_CONFIG_HOME:-$HOME/.config}/dotfiles" ]]; then
+if [[ ! -d "$DOTFILES" ]]; then
 	echo "dotfiles directory not found"
 	exit 255
 fi
 
+# programs that follows XDG
+cd "${XDG_CONFIG_HOME:-$HOME/.config}" || {
+	echo "failed to change the working directory"
+	exit 255
+}
+
+config_xdg=(
+	bat borgmatic.d environment.d fontconfig foot fsh git go jrnl Kvantum less
+	lf litecli luarocks MangoHud mpv mycli npm nvim nvtop pgcli pip pylint rc.d
+	readline sway systemd task tmux zsh bookmarks chrome-flags.conf clang-format
+	dircolors dxvk.conf flake8
+)
+
+for i in "${config_xdg[@]}"; do
+	ln -sf "dotfiles/$i" .
+done
+
+# programs that puts dotfile inside $HOME
+
+cd "$HOME" || {
+	echo "failed to change the working directory"
+	exit 255
+}
+
+ln -sf ".config/dotfiles/bashrc" .bashrc
