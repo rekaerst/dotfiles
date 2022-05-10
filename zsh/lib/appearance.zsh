9db9e61db5e18f0ba6785767c92f1b18d3e32a59
rekaerst <rekaerst@outlook.com>
2022-05-10 13:28:32 +0800

    feat(zsh): use grc for colorized output

--- zsh/lib/appearance.zsh
+++ zsh/lib/appearance.zsh
@@ -8,10 +8,63 @@ eval $(dircolors "$XDG_CONFIG_HOME"/dircolors)
 # Take advantage of $LS_COLORS for completion as well.
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 
-# enable diff color if possible.
-if command diff --color . . &>/dev/null; then
-	alias diff='diff --color'
+# enable applications color
+alias diff='diff --color'
+
+# coloriser
+
+if [[ -n "$TERM" ]] && [[ ! "$TERM" == "dumb" ]] && (( $+commands[grc] )); then
+	cmds=(
+		blkid
+		configure
+		cvs
+		df
+		dig
+		du
+		env
+		fdisk
+		findmnt
+		free
+		getfacl
+		id
+		ip
+		iptables
+		iwconfig
+		last
+		lsattr
+		lsblk
+		lsmod
+		lsof
+		lspci
+		mount
+		netstat
+		nmap
+		ping
+		ping6
+		ps
+		sar
+		sensors
+		ss
+		stat
+		sysctl
+		tcpdump
+		ulimit
+		uptime
+		vmstat
+		whois
+	)
+	for cmd in $cmds ; do
+		if (( $+commands[$cmd] )) ; then
+			# $cmd() {
+			# 	grc --colour=auto ${commands[$0]} "$@"
+			# }
+			
+			alias $cmd="grc $cmd"
+		fi
+	done
+	unset cmd cmds
 fi
+
 
 export GREP_COLORS="mt=31;1"
 
--- zsh/lib/comdef.zsh
+++ zsh/lib/comdef.zsh
@@ -1,2 +1,7 @@
+# user defined functions
+fpath+=($ZDOTDIR/lib/functions)
+autoload -U $ZDOTDIR/lib/functions/*(.:t)
+
 compdef _vim vi
 compdef _command sandbox
+compdef _grc grc
