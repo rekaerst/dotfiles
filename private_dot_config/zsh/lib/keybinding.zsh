0522a47fb9efce63e115f9bdcdef273159062b73
rekaerst <rekaerst@outlook.com>
2022-05-13 02:32:31 +0800

    style(zsh): use ' for bindkey

--- private_dot_config/zsh/lib/keybinding.zsh
+++ private_dot_config/zsh/lib/keybinding.zsh
@@ -1,12 +1,12 @@
-bindkey "^[OP" run-help
+bindkey '^[OP' run-help
 
 # allow ctrl-A and ctrl-E to move to beginning/end of line
-bindkey "^A" beginning-of-line
-bindkey "^E" end-of-line
+bindkey '^A' beginning-of-line
+bindkey '^E' end-of-line
 
 # remove ESC bindings
-bindkey -r "^[,"
-bindkey -r "^[/"
-bindkey -r "^[~"
-bindkey -r "^[^[[C"
-bindkey -r "^[^[[D"
+bindkey -r '^[,'
+bindkey -r '^[/'
+bindkey -r '^[~'
+bindkey -r '^[^[[C'
+bindkey -r '^[^[[D'
