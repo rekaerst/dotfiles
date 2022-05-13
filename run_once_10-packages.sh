4f7b145c4833ac881073d1a86b5261d3a4d5c585
rekaerst <rekaerst@outlook.com>
2022-05-13 12:12:58 +0800

    fix: require wl-clipboard

--- run_once_10-packages.sh
+++ run_once_10-packages.sh
@@ -10,6 +10,7 @@ arch_linux() {
 	packages=(
 		aria2 asp at atop bat bc bear borg borgmatic btop delve duf dust fd
 		foot go lf llvm make mpv neovim nmap nvtop pgcli ripgrep sd rsync
+		wl-clipboard
 	)
 	sudo pacman -S --needed "${packages[@]}"
 }
