6aae994611120c67d3c3c7e1e28b82a7a9335248
rekaerst <rekaerst@outlook.com>
2022-05-13 15:33:00 +0800

    fix(zsh): increase output lines of zsh_stats()

--- private_dot_config/zsh/lib/utils.zsh
+++ private_dot_config/zsh/lib/utils.zsh
@@ -3,7 +3,7 @@
 function zsh_stats() {
 	fc -l 1 \
 		| awk '{ CMD[$2]++; count++; } END { for (a in CMD) print CMD[a] " " CMD[a]*100/count "% " a }' \
-		| grep -v "./" | sort -nr | head -20 | column -c3 -s " " -t | nl
+		| grep -v "./" | sort -nr | head -40 | column -c3 -s " " -t | nl
 }
 
 #
