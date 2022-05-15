c9a8adec5d2d14c177d98f42b0a555ff79e4c86c
rekaerst <rekaerst@outlook.com>
2022-05-15 20:04:33 +0800

    feat: wget xdg

--- private_dot_config/zsh/lib/alias.zsh
+++ private_dot_config/zsh/lib/alias.zsh
@@ -21,3 +21,4 @@ alias hs="python -m http.server"
 alias locate='locate --regex'
 alias R="R --quiet"
 alias jitrocks="luarocks --lua-version 5.1"
+alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
