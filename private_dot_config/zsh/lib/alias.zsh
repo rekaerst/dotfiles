ec2ac87bbb9c7641208239037fedd90c41e37305
rekaerst <rekaerst@outlook.com>
2022-05-18 22:39:04 +0800

    feat(zsh): amd brightness control alias

--- private_dot_config/zsh/lib/alias.zsh
+++ private_dot_config/zsh/lib/alias.zsh
@@ -22,3 +22,4 @@ alias locate='locate --regex'
 alias R="R --quiet"
 alias jitrocks="luarocks --lua-version 5.1"
 alias wget='wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
+bamd() {brightnessctl -d 'amdgpu*' s "${1}%"}
