3a63e5558eebdc31ce28e21c8ad71faf6eb100de
rekaerst <rekaerst@outlook.com>
2022-05-23 21:51:43 +0800

    refactor: drop all scripts

--- run_once_20-setfacl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# nginx server
-if [[ -d "$HOME/srv" ]] && [[ -d "$HOME/src" ]] && [[ -d "$HOME/src/local" ]]; then
-	setfacl -m "g:http:r-x" "$HOME" "$HOME/srv" "$HOME/src" "$HOME/src/local"
-fi
