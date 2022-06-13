cc43447a7d507361fab9be2d6ca08d94208b92ab
rekaerst <rekaersp@gmail.com>
2022-06-14 00:46:27 +0800

    feat: use zoxide for smarter cd

--- private_dot_config/zsh/lib/init.zsh
+++ private_dot_config/zsh/lib/init.zsh
@@ -1,12 +1,13 @@
 SRC="$(dirname "$0")"
 source $SRC/utils.zsh
+source $SRC/options.zsh
 source $SRC/appearance.zsh
 source $SRC/p10k.zsh
 source $SRC/alias.zsh
+source $SRC/directory.zsh
 source $SRC/termtitle.zsh
-source $SRC/options.zsh
-source $SRC/completion.zsh
 source $SRC/keybinding.zsh
+source $SRC/completion.zsh
 source $SRC/fzf.zsh
 [[ -f "$SRC/secrets.zsh" ]] && source $SRC/secrets.zsh
 [[ -n "$SANDBOX" ]] && source $SRC/sandbox.zsh
